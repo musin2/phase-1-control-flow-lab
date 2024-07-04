e86dc0a51e28802b4dfbe3be2e9fd6a69a61ee4c
musin2 <muhsindaud75@gmail.com>
2024-07-04 18:19:10 +0300

    adds lab code

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetD){
+
+  if(feetD <= 400){
+    return 'This one is on me!';
+  } else if(feetD <=2000 ){
+    return 'That will be twenty bucks.';
+  } else if(feetD <= 2500 ){
+    return 'I will gladly take your thirty bucks.';
+  } else if(feetD >= 2501 ){
+    return 'No can do.';
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(destinationCity){
+   let result1 = destinationCity === 'NYC' ? ('Ok, sounds good.') : ('No go.');
+  return result1;
+
+   // if(destinationCity === 'NYC'){
+//   return 'Ok, sounds good.';
+// } else { 
+//   return 'No go.'
+// }
+  }
+
+function switchOnCharmFromTip(tipV){
+  switch(tipV){
+    case 'generous':
+        return "Thank you so much.";
+      break;
+    case 'not as generous':
+        return "Thank you.";
+      break;
+      default:
+        return "Bye.";
+      break;
+  }
 }
